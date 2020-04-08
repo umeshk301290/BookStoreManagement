33fea6d5db593d585da56f901bdf4dc6b11b4242
umeshk301290 <umeshk301290@gmail.com>
2020-04-08 19:28:50 +0530

    remove unused imports

--- src/main/java/com/bookstore/bookstoreapplication/BookStoreApplication.java
+++ src/main/java/com/bookstore/bookstoreapplication/BookStoreApplication.java
@@ -2,13 +2,6 @@ package com.bookstore.bookstoreapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.filter.ShallowEtagHeaderFilter;
-import javax.servlet.Filter;
-
-
-
-
 
 @SpringBootApplication
 public class BookStoreApplication {
@@ -16,6 +9,5 @@ public class BookStoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookStoreApplication.class, args);
 	}
-	
 
 }
